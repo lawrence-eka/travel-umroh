1ff5909854dee88b8fc2a918116854790697493c
Lawrence Eka <lawrence.eka@gmail.com>
2017-04-20 21:41:17 +0400

    a bit of user management.

--- resources/users/beforerequest.js
+++ resources/users/beforerequest.js
@@ -1,5 +1,3 @@
-//console.log("masuk sini");
-//console.log(this);
 var needApproval = {};
 if(this.hasOwnProperty("isTravelAgent"))
 {
@@ -11,18 +9,16 @@ if(this.hasOwnProperty("isAdmin"))
     needApproval.isAdmin = this.isAdmin;
     delete this.isAdmin;
 }
-//console.log(Object.getOwnPropertyNames(needApproval));
+
 if(Object.getOwnPropertyNames(needApproval).length > 0) 
 {
     needApproval.requestedBy = me.id;
     needApproval.requestedOn = (new Date()).getTime();
     if(me.isAdmin) needApproval.isApproved = true;
     this.needApproval = needApproval;
-    //console.log(needApproval);
 }
 
 if(this.email) this.email = this.email.toLowerCase();
 if(this.username) this.username = this.username.toLowerCase();
 if(query && (query.checkAvailability || query.checkIsLastAdmin)) query.$fields = {"email":1, "username":1};
 
-//console.log("keluar sini");
