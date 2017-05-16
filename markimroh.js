413dc029d1c7153df13dcec5f1653938f8a8cfc8
Lawrene Eka <lawrence.eka@gmail.com>
2017-05-16 15:08:29 +0000

    comit from production server

--- markimroh.js
+++ markimroh.js
@@ -1,33 +1,81 @@
 // production.js
-var deployd = require('deployd')
-  , fs = require('fs')
+var deployd = require("deployd")
+  , tls = require("tls") // eka
+  , fs = require("fs")
 ;
+
+var certs = {
+  "localhost": {
+	  key: fs.readFileSync("privkey.pem"),
+	  cert: fs.readFileSync("fullchain.pem") 
+  },
+  "markimroh.tk": {
+	  key: fs.readFileSync("privkey-markimroh.pem"),
+	  cert: fs.readFileSync("fullchain-markimroh.pem") 
+  },
+  "www.markimroh.tk": {
+	  key: fs.readFileSync("privkey-markimroh.pem"),
+	  cert: fs.readFileSync("fullchain-markimroh.pem") 
+  },
+  "travel-umroh.tk": {
+	  key: fs.readFileSync("privkey-travel-umroh.pem"),
+	  cert: fs.readFileSync("fullchain-travel-umroh.pem") 
+  },
+  "www.travel-umroh.tk": {
+	  key: fs.readFileSync("privkey-travel-umroh.pem"),
+	  cert: fs.readFileSync("fullchain-travel-umroh.pem") 
+  }
+}
 
 var server = deployd({
   port: process.env.PORT || 443,
   
-  key: fs.readFileSync('privkey.pem'), // eka
-  cert: fs.readFileSync('fullchain.pem'), // eka
+  // eka starts
+  SNICallback: function(hostname, cb) 
+  {
+	  if (certs[hostname]) 
+	  {
+		if (cb)
+		{
+			var ctx = tls.createSecureContext(certs[hostname])
+			cb(null, ctx);
+			//cb(null, certs[hostname]);
+		} else 
+		{
+			// compatibility for older versions of node
+			return certs[hostname]; 
+		}
+	  } 
+	  else 
+	  {
+		  console.log("No keys/certificates for domain requested:" + hostname);
+		  throw new Error("No keys/certificates for domain requested");
+	  }
+  },
+// eka ends
 
-  env: 'production',
+  key: fs.readFileSync("privkey.pem"), // eka
+  cert: fs.readFileSync("fullchain.pem"), // eka
+
+  env: "production",
   db: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 27105,
-    name: 'deployd',
+    name: "deployd",
     credentials: {
-      username: 'deployd',
-      password: 'd3pl0yd'
+      username: "deployd",
+      password: "d3pl0yd"
     }
   }
 });
 
 server.listen();
 
-server.on('listening', function() {
+server.on("listening", function() {
   console.log("Server is listening");
 });
 
-server.on('error', function(err) {
+server.on("error", function(err) {
   console.error(err);
   process.nextTick(function() { // Give the server a chance to return an error
     process.exit();
