7aa29027f33466d2470461b4a64547e23aadbc2a
Lawrence Eka <lawrence.eka@gmail.com>
2017-07-19 22:50:38 +0400

    uglification dropped for now as at least Manage User failed when uglified.

--- public/dist/travel-agent/home.js
+++ public/dist/travel-agent/home.js
@@ -1,1 +1,213 @@
-yalla.framework.addComponent("/dist/travel-agent/home",function(){function ComponentEvent(type,data,target,currentTarget){this.data=data,this.target=target,this.type=type,this.currentTarget=currentTarget}function initState(){return{}}function onPropertyChange(){}function initState(){return{alert:new Alert,editTravelAgentId:null,isEditMode:!1}}function onPropertyChange(props){props.isEditMode&&(this.state.isEditMode=props.isEditMode.newValue)}function onAddTA(){this.state.editTravelAgentId=null,this.state.isEditMode=!0,$patchChanges()}function onCloseEditor(){this.state.editTravelAgentId=null,this.state.isEditMode=!1,$patchChanges()}function onEditTA(event){this.state.editTravelAgentId=event.data,this.state.isEditMode=!0,$patchChanges()}function onShowPackages(event){window.location.hash="#app/package.home:travelAgentId="+event.data}function $render(_props){_context.list=$inject("/travel-agent/list"),_context.list,_context["edit-travel-agent"]=$inject("/travel-agent/edit-travel-agent"),_context["edit-travel-agent"],_context.entry=$inject("/component/entry"),_context.entry,_context.panel=$inject("/component/panel"),_context.panel,_elementOpenStart("div",""),_attr("element","dist.travel-agent.home"),_elementOpenEnd("div");var _component=IncrementalDOM.currentElement(),_validComponent=yalla.framework.validComponentName(_component,_elementName);_component._state=_component._state&&_validComponent?_component._state:initState.bind(_component)(_props),_component._state._name=_elementName;var _state=_component._state,_self={component:_component,properties:_props,state:_component._state};if(_validComponent&&yalla.framework.propertyCheckChanges(_component._properties,_props,onPropertyChange.bind(_self)),_component._properties=_props,_state.isEditMode&&(_params={travelAgentId:_state.editTravelAgentId,onclose:function(event){var self={target:event.target};self.properties=_props,"elements"in self.target&&(self.elements=self.target.elements),self.currentTarget=this==event.target?self.target:_parentComponent(event.currentTarget),self.component=_component,self.component._state=self.component._state||{},self.state=self.component._state,self.emitEvent=function(eventName,data){var event=new ComponentEvent(eventName,data,self.target,self.currentTarget);"on"+eventName in _props&&_props["on"+eventName](event)},onCloseEditor.bind(self)()}},_context["edit-travel-agent"].render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){})),!_state.isEditMode){_elementOpenStart("span",""),_elementOpenEnd("span"),_params={nofooter:"nofooter",title:"My Travel Agencies"},_context.panel.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(slotName){if("body"===slotName){_elementOpenStart("span",""),_elementOpenEnd("span");var _params={type:"button",naked:"naked",value:"Add New Travel Agency",onclick:function(event){var self={target:event.target};self.properties=_props,"elements"in self.target&&(self.elements=self.target.elements),self.currentTarget=this==event.target?self.target:_parentComponent(event.currentTarget),self.component=_component,self.component._state=self.component._state||{},self.state=self.component._state,self.emitEvent=function(eventName,data){var event=new ComponentEvent(eventName,data,self.target,self.currentTarget);"on"+eventName in _props&&_props["on"+eventName](event)},onAddTA.bind(self)()}};_context.entry.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){}),_elementClose("span")}});var _params={oneditTA:function(event){var self={target:event.target};self.properties=_props,"elements"in self.target&&(self.elements=self.target.elements),self.currentTarget=this==event.target?self.target:_parentComponent(event.currentTarget),self.component=_component,self.component._state=self.component._state||{},self.state=self.component._state,self.emitEvent=function(eventName,data){var event=new ComponentEvent(eventName,data,self.target,self.currentTarget);"on"+eventName in _props&&_props["on"+eventName](event)},onEditTA.bind(self)(event)},onshowPackages:function(event){var self={target:event.target};self.properties=_props,"elements"in self.target&&(self.elements=self.target.elements),self.currentTarget=this==event.target?self.target:_parentComponent(event.currentTarget),self.component=_component,self.component._state=self.component._state||{},self.state=self.component._state,self.emitEvent=function(eventName,data){var event=new ComponentEvent(eventName,data,self.target,self.currentTarget);"on"+eventName in _props&&_props["on"+eventName](event)},onShowPackages.bind(self)(event)}};_context.list.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){}),_elementClose("span")}_elementClose("div")}var $patchChanges=yalla.framework.renderToScreen,$inject=yalla.framework.createInjector("/dist/travel-agent/home"),$export={},_elementName="dist.travel-agent.home",_context={},_parentComponent=yalla.framework.getParentComponent,_merge=yalla.utils.merge,_elementClose=(IncrementalDOM.elementOpen,IncrementalDOM.elementClose),_elementOpenStart=IncrementalDOM.elementOpenStart,_elementOpenEnd=IncrementalDOM.elementOpenEnd,_attr=(IncrementalDOM.elementVoid,IncrementalDOM.text,IncrementalDOM.attr);return IncrementalDOM.skip,"function"==typeof $render&&($export.render=$render),$export}());
+yalla.framework.addComponent("/dist/travel-agent/home", (function() {
+  var $patchChanges = yalla.framework.renderToScreen;
+  var $inject = yalla.framework.createInjector("/dist/travel-agent/home");
+  var $export = {};
+  var $path = "/dist/travel-agent/home";
+  var _elementName = "dist.travel-agent.home";
+  var _context = {};
+  var _parentComponent = yalla.framework.getParentComponent;
+  var _merge = yalla.utils.merge;
+
+  function ComponentEvent(type, data, target, currentTarget) {
+    this.data = data;
+    this.target = target;
+    this.type = type;
+    this.currentTarget = currentTarget;
+  }
+
+  var _elementOpen = IncrementalDOM.elementOpen;
+  var _elementClose = IncrementalDOM.elementClose;
+  var _elementOpenStart = IncrementalDOM.elementOpenStart;
+  var _elementOpenEnd = IncrementalDOM.elementOpenEnd;
+  var _elementVoid = IncrementalDOM.elementVoid;
+  var _text = IncrementalDOM.text;
+  var _attr = IncrementalDOM.attr;
+  var _skip = IncrementalDOM.skip;
+
+  function initState(props) {
+    return {}
+  };
+
+  function onPropertyChange(event) {};
+
+
+  function initState(props) {
+    return {
+      alert: new Alert(),
+      editTravelAgentId: null,
+      isEditMode: false,
+    }
+  }
+
+  function onPropertyChange(props) {
+    debugger;
+    if (props.isEditMode) this.state.isEditMode = props.isEditMode.newValue;
+  }
+
+  function onAddTA() {
+    debugger;
+    this.state.editTravelAgentId = null;
+    this.state.isEditMode = true;
+    $patchChanges();
+  }
+
+  function onCloseEditor() {
+    this.state.editTravelAgentId = null;
+    this.state.isEditMode = false;
+    $patchChanges();
+  }
+
+  function onEditTA(event) {
+    this.state.editTravelAgentId = event.data;
+    this.state.isEditMode = true;
+    $patchChanges();
+  }
+
+  function onShowPackages(event) {
+    window.location.hash = "#app/package.home:travelAgentId=" + event.data;
+  }
+
+  function $render(_props, _slotView) {
+    _context["list"] = $inject("/travel-agent/list");
+    var list = _context["list"];
+    _context["edit-travel-agent"] = $inject("/travel-agent/edit-travel-agent");
+    var editTravelAgent = _context["edit-travel-agent"];
+    _context["entry"] = $inject("/component/entry");
+    var entry = _context["entry"];
+    _context["panel"] = $inject("/component/panel");
+    var panel = _context["panel"];
+    _elementOpenStart("div", "");
+    _attr("element", "dist.travel-agent.home");
+    _elementOpenEnd("div");
+    var _component = IncrementalDOM.currentElement();
+    var _validComponent = yalla.framework.validComponentName(_component, _elementName)
+    _component._state = _component._state && _validComponent ? _component._state : initState.bind(_component)(_props);
+    _component._state._name = _elementName;
+    var _state = _component._state;
+    var _self = {
+      component: _component,
+      properties: _props,
+      state: _component._state
+    };
+    if (_validComponent) {
+      yalla.framework.propertyCheckChanges(_component._properties, _props, onPropertyChange.bind(_self));
+    }
+    _component._properties = _props;
+    if (_state.isEditMode) {
+      var _params = {
+        "travelAgentId": _state.editTravelAgentId,
+        "onclose": function(event) {
+          var self = {
+            target: event.target
+          };
+          self.properties = _props;
+          if ('elements' in self.target) {
+            self.elements = self.target.elements;
+          }
+          self.currentTarget = this == event.target ? self.target : _parentComponent(event.currentTarget);
+          self.component = _component;
+          self.component._state = self.component._state || {};
+          self.state = self.component._state;
+          self.emitEvent = function(eventName, data) {
+            var event = new ComponentEvent(eventName, data, self.target, self.currentTarget);
+            if ('on' + eventName in _props) {
+              _props['on' + eventName](event);
+            }
+          };
+          onCloseEditor.bind(self)();
+        }
+      };
+      _context["edit-travel-agent"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+    }
+    if (!_state.isEditMode) {
+      _elementOpenStart("span", "");
+      _elementOpenEnd("span");
+      var _params = {
+        "nofooter": "nofooter",
+        "title": "My Travel Agencies"
+      };
+      _context["panel"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {
+        if (slotName === "body") {
+          _elementOpenStart("span", "");
+          _elementOpenEnd("span");
+          var _params = {
+            "type": "button",
+            "naked": "naked",
+            "value": "Add New Travel Agency",
+            "onclick": function(event) {
+              var self = {
+                target: event.target
+              };
+              self.properties = _props;
+              if ('elements' in self.target) {
+                self.elements = self.target.elements;
+              }
+              self.currentTarget = this == event.target ? self.target : _parentComponent(event.currentTarget);
+              self.component = _component;
+              self.component._state = self.component._state || {};
+              self.state = self.component._state;
+              self.emitEvent = function(eventName, data) {
+                var event = new ComponentEvent(eventName, data, self.target, self.currentTarget);
+                if ('on' + eventName in _props) {
+                  _props['on' + eventName](event);
+                }
+              };
+              onAddTA.bind(self)();
+            }
+          };
+          _context["entry"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+          _elementClose("span");
+        }
+      });
+      var _params = {
+        "oneditTA": function(event) {
+          var self = {
+            target: event.target
+          };
+          self.properties = _props;
+          if ('elements' in self.target) {
+            self.elements = self.target.elements;
+          }
+          self.currentTarget = this == event.target ? self.target : _parentComponent(event.currentTarget);
+          self.component = _component;
+          self.component._state = self.component._state || {};
+          self.state = self.component._state;
+          self.emitEvent = function(eventName, data) {
+            var event = new ComponentEvent(eventName, data, self.target, self.currentTarget);
+            if ('on' + eventName in _props) {
+              _props['on' + eventName](event);
+            }
+          };
+          onEditTA.bind(self)(event);
+        },
+        "onshowPackages": function(event) {
+          var self = {
+            target: event.target
+          };
+          self.properties = _props;
+          if ('elements' in self.target) {
+            self.elements = self.target.elements;
+          }
+          self.currentTarget = this == event.target ? self.target : _parentComponent(event.currentTarget);
+          self.component = _component;
+          self.component._state = self.component._state || {};
+          self.state = self.component._state;
+          self.emitEvent = function(eventName, data) {
+            var event = new ComponentEvent(eventName, data, self.target, self.currentTarget);
+            if ('on' + eventName in _props) {
+              _props['on' + eventName](event);
+            }
+          };
+          onShowPackages.bind(self)(event);
+        }
+      };
+      _context["list"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+      _elementClose("span");
+    }
+    _elementClose("div");
+  }
+  if (typeof $render === "function") {
+    $export.render = $render;
+  }
+  return $export;
+})());
--- public/dist/user/forgot-password.js
+++ public/dist/user/forgot-password.js
@@ -1,1 +1,200 @@
-yalla.framework.addComponent("/dist/user/forgot-password",function(){function ComponentEvent(type,data,target,currentTarget){this.data=data,this.target=target,this.type=type,this.currentTarget=currentTarget}function initState(){return{}}function onPropertyChange(){}function initState(){return{alert:new Alert(null,$patchChanges,"alert"),success:!1}}function onResetPassword(){var self=this,q={email:this.target.form.elements.email.value},self=this;dpd.resetpassword.post(q,q,function(result,err){self.state.alert.alert(err),err||(self.state.success=!0,$patchChanges())})}function onCancel(){window.location.hash="#app/user.login-form"}function $render(_props){_context.profile=$inject("/user/userProfile"),_context.profile,_context.panel=$inject("/component/panel"),_context.panel,_context.entry=$inject("/component/entry"),_context.entry,_context.alert=$inject("/component/alert"),_context.alert,_elementOpenStart("div",""),_attr("element","dist.user.forgot-password"),_attr("class","container small-margin-top"),_elementOpenEnd("div");var _component=IncrementalDOM.currentElement(),_validComponent=yalla.framework.validComponentName(_component,_elementName);_component._state=_component._state&&_validComponent?_component._state:initState.bind(_component)(_props),_component._state._name=_elementName;var _state=_component._state,_self={component:_component,properties:_props,state:_component._state};_validComponent&&yalla.framework.propertyCheckChanges(_component._properties,_props,onPropertyChange.bind(_self)),_component._properties=_props;var _params={};_context.panel.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(slotName){if("title"===slotName&&(_elementOpenStart("div",""),_elementOpenEnd("div"),_elementOpenStart("strong",""),_elementOpenEnd("strong"),_text("Forgot Your Password?"),_elementClose("strong"),_elementClose("div")),"body"===slotName){_elementOpenStart("div",""),_elementOpenEnd("div"),_elementOpenStart("form",""),_elementOpenEnd("form"),_elementOpenStart("div",""),_attr("class","row"),_elementOpenEnd("div"),_params={type:"email",name:"email",required:"required",prompt:"Your Email Address"},_context.entry.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){}),_params={type:"button",value:"Reset Password",onclick:function(event){var self={target:event.target};self.properties=_props,"elements"in self.target&&(self.elements=self.target.elements),self.currentTarget=this==event.target?self.target:_parentComponent(event.currentTarget),self.component=_component,self.component._state=self.component._state||{},self.state=self.component._state,self.emitEvent=function(eventName,data){var event=new ComponentEvent(eventName,data,self.target,self.currentTarget);"on"+eventName in _props&&_props["on"+eventName](event)},onResetPassword.bind(self)()}},_context.entry.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){});var _params={type:"button",value:"Cancel",onclick:function(event){var self={target:event.target};self.properties=_props,"elements"in self.target&&(self.elements=self.target.elements),self.currentTarget=this==event.target?self.target:_parentComponent(event.currentTarget),self.component=_component,self.component._state=self.component._state||{},self.state=self.component._state,self.emitEvent=function(eventName,data){var event=new ComponentEvent(eventName,data,self.target,self.currentTarget);"on"+eventName in _props&&_props["on"+eventName](event)},onCancel.bind(self)()}};_context.entry.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){}),_elementClose("div"),_elementClose("form"),_elementClose("div")}"footer"===slotName&&(_elementOpenStart("div",""),_elementOpenEnd("div"),_state.success||(_elementOpenStart("span",""),_elementOpenEnd("span"),_text("Please Enter your email address"),_elementClose("span")),_state.success&&(_elementOpenStart("span",""),_elementOpenEnd("span"),_text("Link to reset your password has been sent to your email."),_elementClose("span")),_elementClose("div"))}),_elementOpenStart("span",""),_elementOpenEnd("span"),yalla.framework.registerRef("alert",IncrementalDOM.currentElement(),function(){var _params={alertType:_state.alert.type.bind(self)(),message:_state.alert.text.bind(self)()};_context.alert.render("object"==typeof arguments[1]?_merge(arguments[1],_params):_params,function(){})})(),_elementClose("span"),_elementClose("div")}var $patchChanges=yalla.framework.renderToScreen,$inject=yalla.framework.createInjector("/dist/user/forgot-password"),$export={},_elementName="dist.user.forgot-password",_context={},_parentComponent=yalla.framework.getParentComponent,_merge=yalla.utils.merge,_elementClose=(IncrementalDOM.elementOpen,IncrementalDOM.elementClose),_elementOpenStart=IncrementalDOM.elementOpenStart,_elementOpenEnd=IncrementalDOM.elementOpenEnd,_text=(IncrementalDOM.elementVoid,IncrementalDOM.text),_attr=IncrementalDOM.attr;return IncrementalDOM.skip,"function"==typeof $render&&($export.render=$render),$export}());
+yalla.framework.addComponent("/dist/user/forgot-password", (function() {
+  var $patchChanges = yalla.framework.renderToScreen;
+  var $inject = yalla.framework.createInjector("/dist/user/forgot-password");
+  var $export = {};
+  var $path = "/dist/user/forgot-password";
+  var _elementName = "dist.user.forgot-password";
+  var _context = {};
+  var _parentComponent = yalla.framework.getParentComponent;
+  var _merge = yalla.utils.merge;
+
+  function ComponentEvent(type, data, target, currentTarget) {
+    this.data = data;
+    this.target = target;
+    this.type = type;
+    this.currentTarget = currentTarget;
+  }
+
+  var _elementOpen = IncrementalDOM.elementOpen;
+  var _elementClose = IncrementalDOM.elementClose;
+  var _elementOpenStart = IncrementalDOM.elementOpenStart;
+  var _elementOpenEnd = IncrementalDOM.elementOpenEnd;
+  var _elementVoid = IncrementalDOM.elementVoid;
+  var _text = IncrementalDOM.text;
+  var _attr = IncrementalDOM.attr;
+  var _skip = IncrementalDOM.skip;
+
+  function initState(props) {
+    return {}
+  };
+
+  function onPropertyChange(event) {};
+
+  function initState() {
+    return {
+      alert: new Alert(null, $patchChanges, "alert"),
+      success: false,
+    }
+  }
+
+  function onResetPassword() {
+    var self = this;
+    var q = {
+      email: this.target.form.elements.email.value,
+    }
+    var self = this;
+    dpd.resetpassword.post(q, q, function(result, err) {
+      self.state.alert.alert(err);
+      if (!err) {
+        self.state.success = true;
+        $patchChanges();
+      }
+    });
+  }
+
+  function onCancel() {
+    window.location.hash = "#app/user.login-form"
+  }
+
+  function $render(_props, _slotView) {
+    _context["profile"] = $inject("/user/userProfile");
+    var profile = _context["profile"];
+    _context["panel"] = $inject("/component/panel");
+    var panel = _context["panel"];
+    _context["entry"] = $inject("/component/entry");
+    var entry = _context["entry"];
+    _context["alert"] = $inject("/component/alert");
+    var alert = _context["alert"];
+    _elementOpenStart("div", "");
+    _attr("element", "dist.user.forgot-password");
+    _attr("class", "container small-margin-top");
+    _elementOpenEnd("div");
+    var _component = IncrementalDOM.currentElement();
+    var _validComponent = yalla.framework.validComponentName(_component, _elementName)
+    _component._state = _component._state && _validComponent ? _component._state : initState.bind(_component)(_props);
+    _component._state._name = _elementName;
+    var _state = _component._state;
+    var _self = {
+      component: _component,
+      properties: _props,
+      state: _component._state
+    };
+    if (_validComponent) {
+      yalla.framework.propertyCheckChanges(_component._properties, _props, onPropertyChange.bind(_self));
+    }
+    _component._properties = _props;
+    var _params = {};
+    _context["panel"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {
+      if (slotName === "title") {
+        _elementOpenStart("div", "");
+        _elementOpenEnd("div");
+        _elementOpenStart("strong", "");
+        _elementOpenEnd("strong");
+        _text("Forgot Your Password?");
+        _elementClose("strong");
+        _elementClose("div");
+      }
+      if (slotName === "body") {
+        _elementOpenStart("div", "");
+        _elementOpenEnd("div");
+        _elementOpenStart("form", "");
+        _elementOpenEnd("form");
+        _elementOpenStart("div", "");
+        _attr("class", "row");
+        _elementOpenEnd("div");
+        var _params = {
+          "type": "email",
+          "name": "email",
+          "required": "required",
+          "prompt": "Your Email Address"
+        };
+        _context["entry"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+        var _params = {
+          "type": "button",
+          "value": "Reset Password",
+          "onclick": function(event) {
+            var self = {
+              target: event.target
+            };
+            self.properties = _props;
+            if ('elements' in self.target) {
+              self.elements = self.target.elements;
+            }
+            self.currentTarget = this == event.target ? self.target : _parentComponent(event.currentTarget);
+            self.component = _component;
+            self.component._state = self.component._state || {};
+            self.state = self.component._state;
+            self.emitEvent = function(eventName, data) {
+              var event = new ComponentEvent(eventName, data, self.target, self.currentTarget);
+              if ('on' + eventName in _props) {
+                _props['on' + eventName](event);
+              }
+            };
+            onResetPassword.bind(self)();
+          }
+        };
+        _context["entry"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+        var _params = {
+          "type": "button",
+          "value": "Cancel",
+          "onclick": function(event) {
+            var self = {
+              target: event.target
+            };
+            self.properties = _props;
+            if ('elements' in self.target) {
+              self.elements = self.target.elements;
+            }
+            self.currentTarget = this == event.target ? self.target : _parentComponent(event.currentTarget);
+            self.component = _component;
+            self.component._state = self.component._state || {};
+            self.state = self.component._state;
+            self.emitEvent = function(eventName, data) {
+              var event = new ComponentEvent(eventName, data, self.target, self.currentTarget);
+              if ('on' + eventName in _props) {
+                _props['on' + eventName](event);
+              }
+            };
+            onCancel.bind(self)();
+          }
+        };
+        _context["entry"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+        _elementClose("div");
+        _elementClose("form");
+        _elementClose("div");
+      }
+      if (slotName === "footer") {
+        _elementOpenStart("div", "");
+        _elementOpenEnd("div");
+        if (!_state.success) {
+          _elementOpenStart("span", "");
+          _elementOpenEnd("span");
+          _text("Please Enter your email address");
+          _elementClose("span");
+        }
+        if (_state.success) {
+          _elementOpenStart("span", "");
+          _elementOpenEnd("span");
+          _text("Link to reset your password has been sent to your email.");
+          _elementClose("span");
+        }
+        _elementClose("div");
+      }
+    });
+    _elementOpenStart("span", "");
+    _elementOpenEnd("span");
+    yalla.framework.registerRef("alert", IncrementalDOM.currentElement(), function() {
+      var _params = {
+        "alertType": _state.alert.type.bind(self)(),
+        "message": _state.alert.text.bind(self)()
+      };
+      _context["alert"].render(typeof arguments[1] === "object" ? _merge(arguments[1], _params) : _params, function(slotName, slotProps) {});
+    })()
+    _elementClose("span");
+    _elementClose("div");
+  }
+  if (typeof $render === "function") {
+    $export.render = $render;
+  }
+  return $export;
+})());
