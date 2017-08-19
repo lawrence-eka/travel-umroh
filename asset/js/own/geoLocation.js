ecbc02e2b8b8c3638127a762093e6d4e93063a91
Lawrence Eka <lawrence.eka@gmail.com>
2017-08-19 18:04:19 +0400

    In trying to display map on rather old ios devices.

--- asset/js/own/geoLocation.js
+++ asset/js/own/geoLocation.js
@@ -126,7 +126,7 @@ function GeoLocation() {
 					    strokeOpacity: 0.7,
 					    strokeWeight: 2
 					}));
-					self.qiblaLine.setMap(map);
+					self.qiblaLine.setMap(self.map);
 				}
 /**/
 				options = options || {enableHighAccuracy: true, timeout: 10000};
