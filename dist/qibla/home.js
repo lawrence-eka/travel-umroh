3e832f70f291a19d92b3884fe8b28d21540a16bf
Lawrence Eka <lawrence.eka@gmail.com>
2017-08-19 18:01:14 +0400

    In trying to display map on rather old ios devices.

--- dist/qibla/home.js
+++ dist/qibla/home.js
@@ -39,7 +39,7 @@ yalla.framework.addComponent("/dist/qibla/home", (function() {
     geo.showLocation(map, true, onAlert);
     //}
   }
-  setTimeout(initMap, 0);
+  setTimeout(initMap, 1000);
 
   function initState(props) {
     var state = {
